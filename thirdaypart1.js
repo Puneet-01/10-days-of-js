5c0fb28fb8f57e17cdf8d7651739744658f1009a
Puneet-01 <76750491+Puneet-01@users.noreply.github.com>
2021-10-06 00:36:23 +0530

    Day3:Arrays

---
+++ thirdaypart1.js
@@ -0,0 +1,61 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    
+    const n=nums.length;
+    var max=nums[0];
+    for (let e of nums)
+    {
+        if(e>max)
+        {
+            max=e;
+        }
+    }
+    var max2=nums[0];
+    for(let e of nums)
+    {
+        if(e>max2 && e!=max)
+        {
+            max2=e;
+            
+        }
+    }
+    return max2;
+}
+
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
