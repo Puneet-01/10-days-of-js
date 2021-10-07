e76b4c1b516e280cbc270d5d5428af694fac9638
Puneet-01 <76750491+Puneet-01@users.noreply.github.com>
2021-10-08 01:13:37 +0530

    Day5:Arrow Functions

---
+++ fifthpart3.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+function modifyArray(nums) {
+    
+   let modified = nums.map(num => {
+       if(num%2===1)
+       return num*3;
+       else
+       return num*2;
+      
+   });
+   return modified;
+}
+
+
+function main() {
+    const n = +(readLine());
+    const a = readLine().split(' ').map(Number);
+    
+    console.log(modifyArray(a).toString().split(',').join(' '));
+}
